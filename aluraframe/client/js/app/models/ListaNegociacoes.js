bc46536e0d8cba003153c4d95ae0480c3979b351
Felipe Rodrigues <felipe_rodriguesx@hotmail.com>
2019-02-25 22:25:24 -0300

    automatizando o update da view

--- aluraframe/client/js/app/models/ListaNegociacoes.js
+++ aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,14 +1,21 @@
 class ListaNegociacoes {
 
-    constructor(){
+    constructor(armadilha){
         this._negociocoes = [];
+        this._armadilha = armadilha;
     }
 
     adiciona(negociacao){
         this._negociocoes.push(negociacao);
+        this._armadilha(this);
     }
 
     get negociacoes(){
         return [].concat(this._negociocoes);
+    }
+
+    esvazia() {
+        this._negociocoes = [];
+        this._armadilha(this);
     }
 }
