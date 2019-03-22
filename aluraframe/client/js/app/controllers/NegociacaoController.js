dcc21104e63ce7b70ed7599d571d15ba07b6aea2
Felipe Rodrigues <felipe_rodriguesx@hotmail.com>
2019-03-21 23:04:56 -0300

    Data Bind and refactor Factory

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -7,52 +7,28 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
 
-        let self = this;
-
-        this._listaNegociacoes = new Proxy(new ListaNegociacoes(),{
-            get(target, property, receiver){
-                if(['adiciona', 'esvazia'].includes(property) && typeof(target[property]) == typeof(Function)){
-                    return function(){
-
-                        console.log(`interceptado ${property}`);
-                        
-                        Reflect.apply(target[property], target, arguments);
-                        self._negociacoesView.update(target);
-                    }
-                }
-                return Reflect.get(target, property, receiver);
-            }
-        });
-
-        // this._listaNegociacoes = new ListaNegociacoes(model => {
-        //     this._negociacoesView.update(model);
-        // }); 
-        // Código antigo! Com armadilha
+        this._listaNegociacoes = new Bind(
+            new ListaNegociacoes(), 
+            new NegociacoesView($('#negociacoesView')), 
+            'adiciona', 'esvazia');
         
-        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update(this._listaNegociacoes);
+        this._mensagem = new Bind(
+            new Mensagem, 
+            new MensagemView($('#mensagemView')), 
+            'texto');
 
-        this._mensagem = new Mensagem();
-        this._mensagemView = new MensagemView($('#mensagemView'));
-        this._mensagemView.update(this._mensagem);
     }
 
     adiciona(event){
         event.preventDefault();
-
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-
         this._mensagem.texto = 'Negociação adicianada com sucesso';
-        this._mensagemView.update(this._mensagem)
-
         this._limpaFormulario();
     }
 
     apaga() {
         this._listaNegociacoes.esvazia();
-
         this._mensagem.texto = 'Negociações apagadas com sucesso';
-        this._mensagemView.update(this._mensagem);
     }
 
     _criaNegociacao(){
